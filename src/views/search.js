354678d2d427eb3ce12ed78337672fc49c4ebf93
ozturkselim99 <ozturkselim99@gmail.com>
2021-12-07 15:00:25 +0300

    stillendirme için styled-system kullanıldı

--- src/views/search.js
+++ src/views/search.js
@@ -1,13 +1,16 @@
 import {Button, Text, View} from "react-native";
 import * as React from "react";
+import Box from "../components/box";
+import BoxCenter from "../components/box-center";
 function SearchView({navigation}) {
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <BoxCenter>
             <Button
                 title="Go to Details"
                 onPress={()=>navigation.navigate("Detail")}
             ></Button>
-        </View>
+            <Box size={20} bg={"blue"}/>
+        </BoxCenter>
     );
 }
 
