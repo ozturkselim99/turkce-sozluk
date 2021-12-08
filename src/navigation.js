717493355b433b165ffc2a72aba618025888b067
ozturkselim99 <ozturkselim99@gmail.com>
2021-12-08 12:54:06 +0300

    app.js sadelestirildi

---
+++ src/navigation.js
@@ -0,0 +1,93 @@
+import React from "react";
+import {Text, SafeAreaView, View} from "react-native"
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import {ThemeProvider} from "styled-components";
+import SearchView from "./views/search";
+import HistoryView from "./views/history";
+import FavoriteView from "./views/favorite";
+import DetailView from "./views/detail";
+import TabBar from "./components/tab-bar";
+import Box from "./components/box";
+import theme from "./utils/theme";
+import {SafeAreaProvider} from "react-native-safe-area-context/src/SafeAreaContext";
+import Button from "./components/button";
+import More from "./components/icons/More";
+import Left from "./components/icons/Left";
+
+
+function SettingsScreen() {
+    return (
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <Text>Settings!</Text>
+        </View>
+    );
+}
+
+const Tab = createBottomTabNavigator();
+const HomeStack = createNativeStackNavigator();
+
+function SearchStack() {
+    return (
+        <HomeStack.Navigator
+            screenOptions={{headerShadowVisible: false}}
+        >
+            <HomeStack.Screen
+                options={() => {
+                    return {
+                        headerShown: false,
+                    }
+                }}
+                name="Search" component={SearchView}/>
+            <HomeStack.Screen
+                options={({route, navigation}) => {
+                    return {
+                        title:
+                            (route.params?.title ?? '').slice(0, 15) +
+                            ((route.params?.title ?? '').length > 15 ? '...' : ''),
+                        headerStyle: {
+                            backgroundColor: theme.colors.softRed,
+                        },
+                        headerTitleAlign: "center",
+                        headerLeft: () => (
+                            <Button
+                                height="100%"
+                                onPress={() => navigation.navigate('Search')}
+                            >
+                                <Left width={24} height={24} color={theme.colors.textDark}/>
+                            </Button>
+                        ),
+                        headerRight: () => (
+                            <Button
+                                height="100%"
+                                onPress={() => navigation.navigate('Search')}
+                            >
+                                <More height={24} width={24} color={theme.colors.textDark}/>
+                            </Button>
+                        ),
+                    }
+                }}
+                name="Detail" component={DetailView}/>
+        </HomeStack.Navigator>
+    )
+}
+
+
+function TabNavigator() {
+    return (
+        <NavigationContainer>
+            <Tab.Navigator
+                screenOptions={{
+                    headerShown: false,
+                }}
+                initialRouteName="Search" tabBar={props => <TabBar {...props} />}>
+                <Tab.Screen name="History" component={HistoryView}/>
+                <Tab.Screen name="Search" component={SearchStack}/>
+                <Tab.Screen name="Favorite" component={FavoriteView}/>
+            </Tab.Navigator>
+        </NavigationContainer>
+    )
+}
+
+export default TabNavigator;
--- src/views/detail.js
+++ src/views/detail.js
@@ -1,17 +1,75 @@
 import {View} from "react-native";
 import * as React from "react";
 import Text from "../components/text";
-import {SafeAreaView} from "react-native";
+import {SafeAreaView,ScrollView} from "react-native";
 
 import FocusAwareStatusBar from "../components/focus-aware-status-bar";
 import Box from "../components/box";
 import theme from "../utils/theme";
+import ActionButton, {ActionButtonTitle} from "../components/action-button";
+import More from "../components/icons/More";
+import Favorite from "../components/icons/Favorite";
+import Sound from "../components/icons/Sound";
+import Hand from "../components/icons/Hand";
+import {
+    DetailSummaryItemContainer,
+    DetailSummaryItemSummary,
+    DetailSummaryItemTitle
+} from "../components/detail-summary-item";
+
 function DetailView() {
     return (
 
         <Box as={SafeAreaView} flex={1} bg={theme.colors.softRed} p={16}>
             <FocusAwareStatusBar barStyle="dark-content"  backgroundColor={theme.colors.softRed} />
-            <Text>Detail</Text>
+            <Box as={ScrollView} p={16}>
+            <Box>
+                <Text fontSize={32} fontWeight={"bold"}>Kalem</Text>
+                <Text color={"textLight"} mt={6}>Arapça Kalem</Text>
+            </Box>
+            <Box flexDirection={"row"} mt={24}>
+                <ActionButton >
+                    <Sound color={theme.colors.textLight}></Sound>
+                </ActionButton>
+                <ActionButton ml={12}>
+                    <Favorite  color={theme.colors.textLight}></Favorite>
+                </ActionButton>
+                <ActionButton ml={"auto"}>
+                    <Hand color={theme.colors.textLight}></Hand>
+                    <ActionButtonTitle>Türk İşaret Dili</ActionButtonTitle>
+                </ActionButton>
+            </Box>
+            <Box mt={32}>
+                <DetailSummaryItemContainer>
+                    <DetailSummaryItemTitle>
+                        Yazma, çizme vb. işlerde kullanılan çeşitli biçimlerde araç:
+                    </DetailSummaryItemTitle>
+                    <DetailSummaryItemSummary>
+                        "Kâğıt, kalem, mürekkep, hepsi masanın üstündedir." - Falih Rıfkı
+                        Atay
+                    </DetailSummaryItemSummary>
+                </DetailSummaryItemContainer>
+                <DetailSummaryItemContainer border>
+                    <DetailSummaryItemTitle>
+                        Yazma, çizme vb. işlerde kullanılan çeşitli biçimlerde araç:
+                    </DetailSummaryItemTitle>
+                    <DetailSummaryItemSummary>
+                        "Kâğıt, kalem, mürekkep, hepsi masanın üstündedir." - Falih Rıfkı
+                        Atay
+                    </DetailSummaryItemSummary>
+                </DetailSummaryItemContainer>
+                <DetailSummaryItemContainer border>
+                    <DetailSummaryItemTitle>
+                        Yazma, çizme vb. işlerde kullanılan çeşitli biçimlerde araç:
+                    </DetailSummaryItemTitle>
+                    <DetailSummaryItemSummary>
+                        "Kâğıt, kalem, mürekkep, hepsi masanın üstündedir." - Falih Rıfkı
+                        Atay
+                    </DetailSummaryItemSummary>
+                </DetailSummaryItemContainer>
+            </Box>
+            </Box>
+
         </Box>
     );
 }
